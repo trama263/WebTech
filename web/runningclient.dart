0b8b80c3642877a76c0fd9dc2c96968c01ff13b4
Tram_Nguyen <cyclamenflower@yahoo.com>
2017-06-05 19:05:43 +0200

    add all

--- web/runningclient.dart
+++
@@ -1,7 +0,0 @@
-import 'package:runningdart/runningdart.dart';
-
-void main() {
-  RunningGame model = new RunningGame(gameHeight, gameWidth);
-  RunningView view = new RunningView(model);
-  RunningController controller = new RunningController(model, view);
-}
